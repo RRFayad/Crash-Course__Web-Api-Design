bcf55df6f1015db221a0d56587190346169c8aaf
RRFayad <renan.r.fayad@gmail.com>
2024-05-01 18:34:09 -0300

    feat(oAuth Server): Created Server

---
+++ 4. OAuth Server/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const axios = require("axios");
+const port = process.env.PORT || 3001;
+// const oAuth = require("./middleware/oAuth");
+
+const app = express();
+
+const challengesAPIEndpoint = "http://localhost:8080/challenges";
+
+app.use(oAuth);
+
+app.get("/challenges", async (req, res, next) => {
+  try {
+    const response = await axios({ method: "get", url: challengesAPIEndpoint });
+    res.json(response.data);
+  } catch (err) {
+    console.log(err);
+    if ((err.response.status = 401)) {
+      res.status(401).json("Unauthorized to access data");
+    } else if ((console.err.response.status = 403)) {
+      res.status(403).json("Permission Denied");
+    } else {
+      res.status(500).json("Something went wrong");
+    }
+  }
+});
+
+app.listen((port, () => console.log("Server started")));
